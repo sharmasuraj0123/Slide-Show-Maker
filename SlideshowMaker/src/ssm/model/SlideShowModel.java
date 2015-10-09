848f716a4473ffbd2552123033753f8ff90517cb
Sushi <Sushi@172.24.133.104>
2015-10-09 05:20:52 -0400

    HomeWork 3 Created a directory when the view website is clicked Creates different folders for: images javascripts html and also creates a blank html file

--- SlideshowMaker/src/ssm/model/SlideShowModel.java
+++ SlideshowMaker/src/ssm/model/SlideShowModel.java
@@ -2,7 +2,6 @@ package ssm.model;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.effect.SepiaTone;
 import properties_manager.PropertiesManager;
 import ssm.LanguagePropertyType;
 import ssm.view.SlideShowMakerView;
@@ -10,7 +9,7 @@ import ssm.view.SlideShowMakerView;
 /**
  * This class manages all the data associated with a slideshow.
  * 
- * @author McKilla Gorilla & Suraj Sharma
+ * @author McKilla Gorilla & _____________
  */
 public class SlideShowModel {
     SlideShowMakerView ui;
@@ -27,6 +26,10 @@ public class SlideShowModel {
     // ACCESSOR METHODS
     public boolean isSlideSelected() {
 	return selectedSlide != null;
+    }
+    
+    public boolean isSelectedSlide(Slide testSlide) {
+	return selectedSlide == testSlide;
     }
     
     public ObservableList<Slide> getSlides() {
@@ -49,18 +52,6 @@ public class SlideShowModel {
     public void setTitle(String initTitle) { 
 	title = initTitle; 
     }
-    
-    public Slide getNext(){
-        if(slides.indexOf(selectedSlide) != slides.size()-1)
-        selectedSlide= slides.get(slides.indexOf(selectedSlide)+1);
-        return selectedSlide;
-    }
-    
-    public Slide getPrev(){
-        if(slides.indexOf(selectedSlide) !=0)
-        selectedSlide = slides.get(slides.indexOf(selectedSlide)-1);
-        return selectedSlide;
-    }
 
     // SERVICE METHODS
     
@@ -78,44 +69,99 @@ public class SlideShowModel {
      * Adds a slide to the slide show with the parameter settings.
      * @param initImageFileName File name of the slide image to add.
      * @param initImagePath File path for the slide image to add.
+     * @param initCaption Caption for the slide image to add.
      */
     public void addSlide(   String initImageFileName,
-			    String initImagePath , String initCaption) {
-	Slide slideToAdd = new Slide(initImageFileName, initImagePath , initCaption);
+			    String initImagePath,
+			    String initCaption) {
+	Slide slideToAdd = new Slide(initImageFileName, initImagePath, initCaption);
 	slides.add(slideToAdd);
-	selectedSlide = slideToAdd;
-	ui.reloadSlideShowPane(this);
+	ui.reloadSlideShowPane();
+    }
+
+    /**
+     * Removes the currently selected slide from the slide show and
+     * updates the display.
+     */
+    public void removeSelectedSlide() {
+	if (isSlideSelected()) {
+	    slides.remove(selectedSlide);
+	    selectedSlide = null;
+	    ui.reloadSlideShowPane();
+	}
+    }
+ 
+    /**
+     * Moves the currently selected slide up in the slide
+     * show by one slide.
+     */
+    public void moveSelectedSlideUp() {
+	if (isSlideSelected()) {
+	    moveSlideUp(selectedSlide);
+	    ui.reloadSlideShowPane();
+	}
     }
     
-    public void removeSlide( Slide selectedSlide) {
-	slides.remove(selectedSlide);
-	ui.reloadSlideShowPane(this);
+    // HELPER METHOD
+    private void moveSlideUp(Slide slideToMove) {
+	int index = slides.indexOf(slideToMove);
+	if (index > 0) {
+	    Slide temp = slides.get(index);
+	    slides.set(index, slides.get(index-1));
+	    slides.set(index-1, temp);
+	}
     }
     
-    public void moveUp(Slide selectedSlide){
-        if(selectedSlide ==slides.get(0))
-        return;
-        
-        
-        int index = slides.indexOf(selectedSlide);
-       Slide temp =  slides.remove(index);
-       slides.add(--index,temp);
-        
-        ui.reloadSlideShowPane(this);
+    /**
+     * Moves the currently selected slide down in the slide
+     * show by one slide.
+     */
+    public void moveSelectedSlideDown() {
+	if (isSlideSelected()) {
+	    moveSlideDown(selectedSlide);
+	    ui.reloadSlideShowPane();
+	}
     }
     
-    public void moveDown(Slide selectedSlide){
-        
-        if(selectedSlide ==slides.get(slides.size()-1))
-        return;
-        
-        int index = slides.indexOf(selectedSlide);
-       Slide temp =  slides.remove(index);
-       slides.add(++index,temp);
-        
-        ui.reloadSlideShowPane(this);
+    // HELPER METHOD
+    private void moveSlideDown(Slide slideToMove) {
+	int index = slides.indexOf(slideToMove);
+	if (index < (slides.size()-1)) {
+	    Slide temp = slides.get(index);
+	    slides.set(index, slides.get(index+1));
+	    slides.set(index+1, temp);
+	}
     }
     
-    
-    
+    /**
+     * Changes the currently selected slide to the previous slide
+     * in the slide show.
+     */
+    public void previous() {
+	if (selectedSlide == null)
+	    return;
+	else {
+	    int index = slides.indexOf(selectedSlide);
+	    index--;
+	    if (index < 0)
+		index = slides.size() - 1;
+	    selectedSlide = slides.get(index);
+	}
+    }
+
+    /**
+     * Changes the currently selected slide to the next slide
+     * in the slide show.
+     */    
+    public void next() {
+    	if (selectedSlide == null)
+	    return;
+	else {
+	    int index = slides.indexOf(selectedSlide);
+	    index++;
+	    if (index >= slides.size())
+		index = 0;
+	    selectedSlide = slides.get(index);
+	}
+    }    
 }
